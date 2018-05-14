42ad13b441f66144b52056c67c9978c8f11ba886
Andy Franklin <andy.franklin@thedrum.com>
2018-05-14 22:22:15 +0100

    Autowiring Question controller

--- src/AndyFranklin/FaqBundle/Controller/AnswerController.php
+++ src/AndyFranklin/FaqBundle/Controller/AnswerController.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Andy
- * Date: 13/05/2018
- * Time: 21:28
- */
 
 namespace AndyFranklin\FaqBundle\Controller;
 
+use FOS\RestBundle\Controller\FOSRestController;
 
-class AnswerController
+class AnswerController extends FOSRestController
 {
 
 }
